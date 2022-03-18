ff27cc016f2268b8ba0e841599283bae38f511c0
Morgan Buntin <mmbuntin@gmail.com>
2022-03-18 14:24:06 -0400

    Added if logic for space in build_game_board

--- snowman/snowman.py
+++ snowman/snowman.py
@@ -93,7 +93,7 @@ def is_word_guessed(word_dict):
 def build_game_board(word, word_dict):
     output_letters = []
     for elem in word:
-        if word_dict[elem]:
+        if elem == " " or word_dict[elem]:
             output_letters += elem
         else:
             output_letters += "_"
